b20b395a7e747372aeb703d1212cc2963c262c9e
RickyLiTHU <srwlsc@126.com>
2018-07-21 22:16:53 +0800

    20180721

---
+++ 49.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hashmap = {}
+        for st in strs:
+            key = ''.join(sorted(st))
+            if key not in hashmap:
+                hashmap[key] = [st]
+            else:
+                hashmap[key] += [st]
+        return hashmap.values()
